1b365db040f44715db6dc81890763d0e2e9fb69a
EliteBco <78278765+jtorress66@users.noreply.github.com>
2026-01-05 16:48:03 -0400

    Finalize storage browser updates and dependency sync

--- app/StorageBrowser.tsx
+++ app/StorageBrowser.tsx
@@ -1,16 +1,7 @@
 import { StorageBrowser } from '@aws-amplify/ui-react-storage';
 
 // these should match access patterns defined in amplify/storage/resource.ts
-const defaultPrefixes = [
-  'ConversionFiles/',
-  'ConversionFileErrors/',
-  'InitialUpload/',
-  'InitialUploadErrors/',
-  'TSQLFiles/',
-  'DataValidation/',
-  //(identityId: string) => `protected/${identityId}/`,
-  //(identityId: string) => `private/${identityId}/`,
-];
+const defaultPrefixes = ['public/'];
 
 export default function Example() {
   return (
